feaf91355243b83eb24fdde432c929151021218a
Stephen Akuffo <stephen.akuffo@gmail.com>
2017-05-06 10:55:30 -0400

    basic ui added

--- server/server.js
+++ server/server.js
@@ -26,11 +26,11 @@ io.on('connection', (socket) => {
 });
 
 const newMessage = (socket) => {
-    socket.on('createMessage', (newMessage) => {
+    socket.on('createMessage', (newMessage, callback) => {
         console.log('createMessage', newMessage);
-
         io.emit('newMessage', generateMessage(newMessage.from, newMessage.text));
 
+        callback('This is from the server');
     });
 }
 
