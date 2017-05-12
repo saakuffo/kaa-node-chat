11ea95f8854234636be9c5b7dc59c4b89d5d98ff
Stephen Akuffo <stephen.akuffo@gmail.com>
2017-05-11 23:14:13 -0400

    autoscrolling

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 const socket = io();
 
+const scrollToBottom = () => {
+    // Selectors
+    const messages = $('#messages');
+    const newMessage = messages.children('li:last-child');
+    // Heights
+    const clientHeight = messages.prop('clientHeight');
+    const scrollTop = messages.prop('scrollTop');
+    const scrollHeight = messages.prop('scrollHeight');
+    const newMessageHeight = newMessage.innerHeight();
+    const lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+        messages.scrollTop(scrollHeight)
+    }
+}
+
 socket.on('connect', () => {
     console.log('Connected to server');
 });
@@ -22,6 +38,7 @@ socket.on('newMessage', (message) => {
     });
 
     $('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (location){
@@ -34,6 +51,7 @@ socket.on('newLocationMessage', function (location){
     });
 
     $('#messages').append(html);
+    scrollToBottom();
 });
 
 $('#message-form').on('submit', (e) => {
